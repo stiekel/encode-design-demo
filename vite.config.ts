cdff82b59e44070e4c061d00d5f5dfb61ccecbc7
chenshidi <chenshidi@bytedance.com>
2025-01-15 20:57:17 +0800

    feat: add dts config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      outDir: 'dist/types',
+      include: [
+        'src/**/*.ts',
+        'src/**/*.d.ts',
+        'src/**/*.tsx'
+      ],
+      tsconfigPath: './tsconfig.app.json'
+    }),
+  ],
+  build: {
+    target: 'modules',
+    outDir: 'dist',
+    lib: {
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        format: 'es',
+        esModule: true,
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  }
 })
